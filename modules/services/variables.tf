4543031fbf11be52788ed601e9ed5c2048d9ab8d
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-18 05:23:11 +0000

    Tf Changes

--- modules/services/variables.tf
+++ modules/services/variables.tf
@@ -7,12 +7,13 @@ variable "service_list" {
     description = "List of Services to be Enabled"
     type = list
     default = [
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
     "logging.googleapis.com",
     "iam.googleapis.com",
     "admin.googleapis.com",
     "appengine.googleapis.com",
     "storage-api.googleapis.com",
-    "cloudbuild.googleapis.com",
 	"containerregistry.googleapis.com",
   ]
 }
